7ea648a31f322a3f77c6162ca49ecff2b1bdb79e
dsfernandez <dsfernandez@gmail.com>
2020-08-23 14:50:40 -0400

    fin datos

---
+++ prueba.R
@@ -0,0 +1,7 @@
+# paquete para leer datos desde Excel
+library(readxl)
+# no tengo idea para que sirve !!!
+library(recipes)  # paquete
+# este es un ejemplo de uso de la función mean para calcular la media
+mean(c(3,5,7,4))
+
